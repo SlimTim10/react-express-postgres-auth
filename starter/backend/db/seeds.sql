9db80dd327267406ccd7c2581dfcd552c12b76ac
SlimTim10 <slimtim10@gmail.com>
2023-01-15 13:50:04 -0500

    Fix seeds

--- starter/backend/db/seeds.sql
+++ starter/backend/db/seeds.sql
@@ -1,17 +1,17 @@
 -- A couple dummy users, Alice and Bob
-INSERT INTO users (id, username, email, password)
-    VALUES (1, 'alice', 'alice@email.com', '1234');
-INSERT INTO users (id, username, email, password)
-    VALUES (2, 'bob', 'bob@email.com', '1234');
+INSERT INTO users (username, email, password)
+    VALUES ('alice', 'alice@email.com', '1234');
+INSERT INTO users (username, email, password)
+    VALUES ('bob', 'bob@email.com', '1234');
 
 -- A couple dummy posts by Alice
-INSERT INTO posts (id, title, body, author_id)
-    VALUES (1, 'First Post', 'This is the body of the first post.', 1);
-INSERT INTO posts (id, title, body, author_id)
-    VALUES (2, 'Second Post', 'This is the body of the second post.', 1);
+INSERT INTO posts (title, body, author_id)
+    VALUES ('First Post', 'This is the body of the first post.', (SELECT id FROM users WHERE username = 'alice'));
+INSERT INTO posts (title, body, author_id)
+    VALUES ('Second Post', 'This is the body of the second post.', (SELECT id FROM users WHERE username = 'alice'));
 
 -- A couple dummy posts by Bob
-INSERT INTO posts (id, title, body, author_id)
-    VALUES (3, 'Third Post', 'This is the body of the third post.', 2);
-INSERT INTO posts (id, title, body, author_id)
-    VALUES (4, 'Fourth Post', 'This is the body of the fourth post.', 2);
+INSERT INTO posts (title, body, author_id)
+    VALUES ('Third Post', 'This is the body of the third post.', (SELECT id FROM users WHERE username = 'bob'));
+INSERT INTO posts (title, body, author_id)
+    VALUES ('Fourth Post', 'This is the body of the fourth post.', (SELECT id FROM users WHERE username = 'bob'));
